eda3f280175a10953eb55dcbe011765fe5e2cc34
masoudmomen <masoudmomen@hotmail.com>
2024-07-02 12:48:39 +0330

    1403-04-12-lap

--- Talakado.Application/BasketsService/IBasketService.cs
+++ Talakado.Application/BasketsService/IBasketService.cs
@@ -112,6 +112,14 @@ namespace Talakado.Application.BasketsService
         public int Id { get; set; }
         public string BuyerId { get; set; }
         public List<BasketItemDto> Items { get; set; } = new List<BasketItemDto>();
+        public int Total()
+        {
+            if (Items.Count>0)
+            {
+                return Items.Sum(p => p.UnitPrice * p.Quantity);
+            }
+            return 0;
+        }
     }
 
     public class BasketItemDto
