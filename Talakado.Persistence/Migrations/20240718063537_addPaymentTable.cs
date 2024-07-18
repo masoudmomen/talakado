c589d385ffc78722c280d69ba8f860ae5716efc1
masoudmomen <masoudmomen@hotmail.com>
2024-07-18 11:50:00 +0330

    1403-04-28-lap

---
+++ Talakado.Persistence/Migrations/20240718063537_addPaymentTable.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Talakado.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addPaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "UserAddresses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 273, DateTimeKind.Local).AddTicks(1913),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 420, DateTimeKind.Local).AddTicks(6933));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 268, DateTimeKind.Local).AddTicks(5337),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 420, DateTimeKind.Local).AddTicks(1263));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 271, DateTimeKind.Local).AddTicks(2939),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 420, DateTimeKind.Local).AddTicks(4614));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogType",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(8031),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(8140));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(9244),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(9083));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogItemImage",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(2744),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(4951));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogItemFeature",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 266, DateTimeKind.Local).AddTicks(7850),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(2534));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogBrand",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(3237),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(6326));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "Baskets",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 264, DateTimeKind.Local).AddTicks(1587),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(1165));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "BasketItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 264, DateTimeKind.Local).AddTicks(7166),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(3637));
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    IsPay = table.Column<bool>(type: "bit", nullable: false),
+                    DatePay = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Authority = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RefId = table.Column<long>(type: "bigint", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    InsertTime = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 272, DateTimeKind.Local).AddTicks(2764)),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    RemoveTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(3237));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(3237));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(3237));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(3237));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(3237));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(8031));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(8031));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(8031));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(8031));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(8031));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderId",
+                table: "Payments",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "UserAddresses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 420, DateTimeKind.Local).AddTicks(6933),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 273, DateTimeKind.Local).AddTicks(1913));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 420, DateTimeKind.Local).AddTicks(1263),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 268, DateTimeKind.Local).AddTicks(5337));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 420, DateTimeKind.Local).AddTicks(4614),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 271, DateTimeKind.Local).AddTicks(2939));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogType",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(8140),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(8031));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(9083),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(9244));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogItemImage",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(4951),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 267, DateTimeKind.Local).AddTicks(2744));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogItemFeature",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(2534),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 266, DateTimeKind.Local).AddTicks(7850));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "CatalogBrand",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(6326),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 265, DateTimeKind.Local).AddTicks(3237));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "Baskets",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(1165),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 264, DateTimeKind.Local).AddTicks(1587));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InsertTime",
+                table: "BasketItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(3637),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 7, 18, 10, 5, 34, 264, DateTimeKind.Local).AddTicks(7166));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(6326));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(6326));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(6326));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(6326));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogBrand",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 418, DateTimeKind.Local).AddTicks(6326));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(8140));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(8140));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(8140));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(8140));
+
+            migrationBuilder.UpdateData(
+                table: "CatalogType",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "InsertTime",
+                value: new DateTime(2024, 7, 15, 19, 0, 40, 419, DateTimeKind.Local).AddTicks(8140));
+        }
+    }
+}
--- Talakado.Web/Controllers/PayController.cs
+++ Talakado.Web/Controllers/PayController.cs
@@ -1,4 +1,4 @@
-﻿using AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Dto.Payment;
 using Microsoft.AspNetCore.Mvc;
 using Talakado.Application.Payments;
