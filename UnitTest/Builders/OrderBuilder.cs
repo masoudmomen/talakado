60a4cb8e7ddeff32d0820be354b9435ff552baf7
masoud momen <masoudmomen@hotmail.com>
2024-08-13 00:56:17 +0330

    1403-05-22-srv

---
+++ UnitTest/Builders/OrderBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talakado.Domain.Order;
+
+namespace UnitTest.Builders
+{
+    public class OrderBuilder
+    {
+        private Order _order;
+        public string TestBuyerId => "58968";
+        public int TestCatalogItemId => 234;
+        public string TestProductName => "Test Product Name";
+        public string TestPictureUri => "https://bugeto.net/images/2021/2021-3/2021-3-";
+        public int TestUnitPrice = 1000;
+        public int TestUnits = 3;
+
+
+        //public Order CreateOrderWithDefaultValue()
+        //{
+        //    List<OrderItem> testOrderItem = new List<OrderItem>()
+        //    {
+        //        new OrderItem(TestCatalogItemId, TestProductName, TestPictureUri, TestUnitPrice, TestUnits)
+        //    };
+        //    return new Order(TestBuyerId, new AddressBuilder().Build(), testOrderItem, PaymentMethod.OnlinePayment, null);
+        //}
+    }
+}
---
+++ UnitTest/Core/Entities/OrderTest/OrderStatusTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Core.Entities.OrderTest
+{
+    public class OrderStatusTest
+    {
+        [Fact]
+        public void When_order_is_delivered_OrderStatus_changes_to_Delivered()
+        {
+
+        }
+    }
+}
