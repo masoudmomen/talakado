b8552bd91738b7bf7cde286a88adc2c8a90f38b6
masoudmomen <masoudmomen@hotmail.com>
2024-08-13 18:08:32 +0330

    1403-5-05-23-lap

---
+++ Talakado.Api/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talakado.Application.Catalogs.CatalogItems.GetCatalogItemPDP;
+using Talakado.Application.Catalogs.CatalogItems.GetCatalogItemPLP;
+
+namespace Talakado.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IGetCatalogItemPLPService getCatalogItemPLPService;
+        private readonly IGetCatalogItemPDPService getCatalogItemPDPService;
+
+        public ProductController(IGetCatalogItemPLPService getCatalogItemPLPService, IGetCatalogItemPDPService getCatalogItemPDPService)
+        {
+            this.getCatalogItemPLPService = getCatalogItemPLPService;
+            this.getCatalogItemPDPService = getCatalogItemPDPService;
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromQuery] CatalogPLPRequestDto request)
+        {
+            return Ok(getCatalogItemPLPService.Execute(request));
+        }
+    }
+}
