b4af0fc6041768ee05c468abaed45605364feda0
masoud momen <masoudmomen@hotmail.com>
2024-12-24 23:48:44 +0330

    1403-10-04-srv

--- Talakado.AdminPanel/Pages/Personels/Index.cshtml.cs
+++ Talakado.AdminPanel/Pages/Personels/Index.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Talakado.Application.ContentManager;
 
 namespace Talakado.AdminPanel.Pages.Personels
 {
     public class IndexModel : PageModel
     {
+        private readonly IPersonelManager personelManager;
+
+        public IndexModel(IPersonelManager personelManager)
+        {
+            this.personelManager = personelManager;
+        }
+        [BindProperty]
+        public List<PersonelDto>? Personels { get; set; }
         public void OnGet()
         {
+            Personels = personelManager.GetPersonelsList();         
+            TempData["Page"] = 11;
         }
     }
 }
